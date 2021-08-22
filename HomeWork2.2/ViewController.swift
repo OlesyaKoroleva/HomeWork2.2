c6b06eda8cc520157ba0443dad6915aacecfcac6
Olesya <favourite89@yandex.ru>
2021-08-22 19:34:09 +0300

    Home Work 2.2

--- HomeWork2.2/ViewController.swift
+++ HomeWork2.2/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var viewColor: UIView!
+        
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewColor.layer.cornerRadius = 10
+        
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        viewColor.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        
     }
+    
+    @IBAction func redValueAction() {
+        let color = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        
+        viewColor.backgroundColor = color
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    
+    @IBAction func greenValueAction() {
+        
+        viewColor.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
 
-
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func blueValueAction() {
+        
+        viewColor.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    
 }
-
